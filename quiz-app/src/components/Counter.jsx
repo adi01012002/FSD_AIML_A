e4603877eb8e769228e72ad57aaff6da8e3ada31
Aditya Sharma <adi01012004@gmail.com>
2024-11-21 15:26:46 +0530

    Counter

--- quiz-app/src/components/Counter.jsx
+++ quiz-app/src/components/Counter.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Counter.css";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
 
   function handleIncrease() {
     setCount(count + 1);
@@ -11,10 +12,15 @@ const Counter = () => {
   function handleDecrease() {
     setCount(count - 1);
   }
+  useEffect(()=>{
+    setCount1(count*5)
+  },[count])
+
 
   return (
     <div className="counter-container">
       <h1 className="counter-heading">Counter: {count}</h1>
+      <h1 className="counter-heading">Counter1: {count1}</h1>
       <div className="counter-buttons">
         <button onClick={handleIncrease}>Increase</button>
         <button onClick={handleDecrease}>Decrease</button>
